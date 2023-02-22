51c9df2ce70c06831f7c744a946c35c27da7933b
crisandryj <crisandryj@gmail.com>
2023-02-22 08:19:50 -0500

    Posts new, index and create added to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
 end
