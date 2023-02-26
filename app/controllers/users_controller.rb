898a3c1ab92adaabc90dec2fe6e8c9ab46d4ef78
crisandryj <crisandryj@gmail.com>
2023-02-26 10:25:34 -0500

    functions added to user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+end
