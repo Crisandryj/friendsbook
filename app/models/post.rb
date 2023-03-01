36845e19106333677803aa253252c58fc006c786
crisandryj <crisandryj@gmail.com>
2023-02-28 19:04:48 -0500

    changed assocaition name

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 
   #trying to create assoicaton for user_comments
   has_many :comments
-  has_many :user_comments, through: :comments, source: :post
+  has_many :commented_users, through: :comments, source: :post
 
   has_many :likes, as: :likeable,  dependent: :destroy
 end
