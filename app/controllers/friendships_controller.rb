eb0bbef17f72de2ae7d0fe134286dd46afd9c8cc
crisandryj <crisandryj@gmail.com>
2023-03-06 20:21:23 -0500

    friendship methods

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,20 @@
 class FriendshipsController < ApplicationController
+  def new
+    @friendship = Friendship.new
+  end
+
+  def create
+    @friendship = Friendship.create(friends_params)
+    if @friendship.save
+      redirect_to @friendship
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def friends_params
+
+  end
 end
