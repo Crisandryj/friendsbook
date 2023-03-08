a043e48b2ad493d0f3080aada04fa9bea19e625c
crisandryj <crisandryj@gmail.com>
2023-03-07 20:16:03 -0500

    friendship error list

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,13 +8,13 @@ class FriendshipsController < ApplicationController
     if @friendship.save
       redirect_to @friendship
     else
-      render new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def friends_params
-
+    params.require(:friendship).permit(:requestee_id, :requestor_id, :accepted)
   end
 end
